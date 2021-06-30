db20e3f81e2f79c2cce32d38016592c907190500
haron.rp <haron.rp@H01-19-03>
2021-06-30 11:38:23 +0800

    Head Widget updates 7 fixes

--- FY19/Models/Widgets/KMJ_HeadHTMLProperties.cs
+++ FY19/Models/Widgets/KMJ_HeadHTMLProperties.cs
@@ -10,18 +10,8 @@ namespace FY19.Models.Widgets
 {
     public class KMJ_HeadHTMLProperties : IWidgetProperties
     {
-        [EditingComponent(TextAreaComponent.IDENTIFIER, Order = 0, Label = "Heading Text")]
-        [EditingComponentProperty(nameof(TextAreaProperties.Name), "")]
-        [StringLength(200, ErrorMessage = "Limit 200 characters")]
+        [EditingComponent(TextAreaComponent.IDENTIFIER, Order = 0, Label = "Heading Text")]    
         [Required]
-        public string Heading_Text { get; set; }
-
-        [EditingComponent(TextInputComponent.IDENTIFIER, Order = 1, Label = "Id")]
-        [StringLength(200, ErrorMessage = "Limit 200 characters")]
-        public string Id { get; set; }
-
-        [EditingComponent(TextInputComponent.IDENTIFIER, Order = 2, Label = "Class")]
-        [StringLength(200, ErrorMessage = "Limit 200 characters")]
-        public string Class { get; set; }
+        public string HeadTags { get; set; }
     }
 }
